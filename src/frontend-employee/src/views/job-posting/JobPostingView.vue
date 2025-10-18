caabadee8c69cf2a58cefd211d6c3dc60e7c7e79
Vuk Perovic <vukperovic03@gmail.com>
2025-10-18 19:35:55 +0200

    Fixed frontend's behavior for deleting job posting.

--- src/frontend-employee/src/views/job-posting/JobPostingView.vue
+++ src/frontend-employee/src/views/job-posting/JobPostingView.vue
@@ -50,8 +50,7 @@ const deleteJob = async () => {
     const response = await deleteJobPosting(job.value.id)
     setSuccessMessage(response.data.message)
   } catch (error) {
-    const message = error.response?.data?.message || error.message || 'Failed deleting job posting'
-    setErrorMessage(message)
+    setErrorMessage('Failed deleting job posting. There could be applications associated with it.')
   }
 }
 
@@ -74,46 +73,60 @@ const setSuccessMessage = (message) => {
 </script>
 
 <template>
+  <div class="container">
 
-  <h1>Job Posting Details</h1>
-  <p v-if="successMessage" class="success-message">{{ successMessage }}</p>
-  <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
-
-  <form>
-    <div class="form-row">
-      <label for="title"><b>Title:</b></label>
-      <input id="title" type="text" v-model="job.title" maxlength="50">
+    <h1>Job Posting Details</h1>
+    <div class="error-container">
+      <p v-if="successMessage" class="success-message">{{ successMessage }}</p>
+      <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
     </div>
 
+    <form>
+      <div class="form-row">
+        <label for="title"><b>Title:</b></label>
+        <input id="title" type="text" v-model="job.title" maxlength="50">
+      </div>
 
-    <div class="form-row">
-      <label for="desc"><b>Description:</b></label>
-      <textarea id="desc" name="description" rows="6" cols="50" maxlength="3000" v-model="job.desc"></textarea>
-    </div>
 
-    <div class="form-row">
-      <label for="published"><b>Published:</b></label>
-      <input id="published" type="date" v-model="job.published" disabled="true">
-    </div>
+      <div class="form-row">
+        <label for="desc"><b>Description:</b></label>
+        <textarea id="desc" name="description" rows="6" cols="50" maxlength="3000" v-model="job.desc"></textarea>
+      </div>
 
-    <div class="form-row">
-      <label for="expires"><b>Expires:</b></label>
-      <input id="expires" type="date" v-model="job.expires" :disabled="job.status === 'CLOSED'">
-    </div>
+      <div class="form-row">
+        <label for="published"><b>Published:</b></label>
+        <input id="published" type="date" v-model="job.published" disabled="true">
+      </div>
 
-    <div class="form-row">
-      <label for="status"><b>Status:</b></label>
-      <span>{{ job.status }}</span>
-    </div>
+      <div class="form-row">
+        <label for="expires"><b>Expires:</b></label>
+        <input id="expires" type="date" v-model="job.expires" :disabled="job.status === 'CLOSED'">
+      </div>
 
-    <div class="form-row">
-      <button type="button" @click="updateJob">Update</button>
-      <button type="button" @click="deleteJob">Delete</button>
-    </div>
-  </form>
+      <div class="form-row">
+        <label for="status"><b>Status:</b></label>
+        <span>{{ job.status }}</span>
+      </div>
+
+      <div class="form-row">
+        <button type="button" @click="updateJob">Update</button>
+        <button type="button" @click="deleteJob">Delete</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
+.error-container {
+  margin-bottom: 20px;
+}
+
+.container {
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+}
+
 .success-message {
   color: green;
   font-weight: bold;
