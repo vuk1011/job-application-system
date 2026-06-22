610c4474a579f854494dbbd45e65bdffefee2c18
Vuk Perovic <vukperovic03@gmail.com>
2026-06-22 11:05:30 +0200

    Job application DTO for employees

--- src/main/java/com/vuk/spring_webapp/transfer/dto/JobApplicationEmployeeDto.java
+++ src/main/java/com/vuk/spring_webapp/transfer/dto/JobApplicationEmployeeDto.java
@@ -1,7 +1,6 @@
 package com.vuk.spring_webapp.transfer.dto;
 
 import com.vuk.spring_webapp.domain.job_application.JobApplicationStatus;
-import com.vuk.spring_webapp.domain.job_posting.JobPosting;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -11,6 +10,6 @@ public class JobApplicationEmployeeDto {
     private Long id;
     private LocalDate dateOfSubmission;
     private JobApplicationStatus status;
-    private JobPosting jobPosting;
+    private JobPostingDto jobPosting;
     private Long candidateId;
 }
