0072d297899dd6fc0dc6cd783f3a4a0d9e9d51ee
Vuk Perovic <vukperovic03@gmail.com>
2026-07-18 17:28:38 +0200

    Tests for Offer entity class

--- src/test/java/com/vuk/spring_webapp/domain/offer/OfferTest.java
+++ src/test/java/com/vuk/spring_webapp/domain/offer/OfferTest.java
@@ -1,50 +1,38 @@
 package com.vuk.spring_webapp.domain.offer;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.vuk.spring_webapp.domain.job_application.JobApplication;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class OfferTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    private static final String NAME = "Initial employment offer";
+    private static final JobApplication JOB_APPLICATION = new JobApplication();
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    @DisplayName("No args constructor creates an empty instance")
+    void noArgsConstructorCreatesEmptyInstance() {
+        Offer offer = new Offer();
+
+        assertNull(offer.getId());
+        assertNull(offer.getName());
+        assertNull(offer.getAccepted());
+        assertNull(offer.getJobApplication());
     }
 
     @Test
-    void getId() {
+    @DisplayName("Parameterized constructor sets fields correctly")
+    void argsConstructorSetsFields() {
+        Offer offer = new Offer(NAME, JOB_APPLICATION);
+
+        assertNull(offer.getId());
+        assertNull(offer.getAccepted());
+
+        assertEquals(NAME, offer.getName());
+        assertEquals(JOB_APPLICATION, offer.getJobApplication());
     }
 
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void getAccepted() {
-    }
-
-    @Test
-    void getJobApplication() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void setAccepted() {
-    }
-
-    @Test
-    void setJobApplication() {
-    }
 }
